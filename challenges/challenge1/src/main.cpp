b85775aab94b78fa56babce30475f211f78acb85
Nils Malmberg <nilsaido.malmberg@mail.polimi.it>
2024-03-10 18:28:57 +0000

    ADD: lambdas function and grad + print results

--- challenges/challenge1/src/main.cpp
+++ challenges/challenge1/src/main.cpp
@@ -1,24 +1,35 @@
 #include <iostream>
+#include <cmath>
 #include "main.hpp"
 #include "json_parser.hpp"
+#include "search_min.hpp"
 
 int main() {
 
     Parameters readParams;
     readParametersFromJson(readParams, "parameters.json");
+    printParametersFromJson(readParams);
 
-    // Print all users parameters
-    std::cout << "Parameters read from JSON file:\n";
-    std::cout << "alpha0: " << readParams.alpha0 << std::endl;
-    std::cout << "mu: " << readParams.mu << std::endl;
-    std::cout << "maxIter: " << readParams.maxIter << std::endl;
-    std::cout << "ltol: " << readParams.lTol << std::endl;
-    std::cout << "rtol: " << readParams.rTol << std::endl;
-    std::cout << "Initial conditions: ";
-    for (const auto& value : readParams.initialConditions) {
-        std::cout << value << " ";
+    auto functionToMinimize = [](const std::vector<double>& x) { // & is used to give a reference, to avoid function to copy x
+        return x[0] * x[1] + 4 * std::pow(x[0],4) + x[1] * x[1] + 3 * x[0];
+    };
+
+     auto functionGradientUserDefine = [](const std::vector<double>& x) -> std::vector<double> {
+        std::vector<double> grad(2);
+        grad[0] = x[1] + 16 * std::pow(x[0],3) + 3;
+        grad[1] = x[0] + 2 * x[1];
+        return grad;
+    };
+
+    std::vector<double> minPoint = searchMinimum(functionToMinimize, functionGradientUserDefine, readParams);
+
+    std::cout << "Minimum coordinates : [ ";
+    for (size_t i = 0; i < minPoint.size(); ++i) {
+        std::cout << minPoint[i] << " ";
     }
-    std::cout << std::endl;
+    std::cout << "]" << std::endl;
+
+    std::cout << "Value of the minimum : " << functionToMinimize(minPoint) << std::endl;
 
     return 0;
 }
