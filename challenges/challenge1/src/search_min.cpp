357b8ed8c032cf76bbfa5f5e3f4799934fc8361f
Nils Malmberg <nilsaido.malmberg@mail.polimi.it>
2024-03-10 19:29:13 +0000

    ADD: learning rate function

--- challenges/challenge1/src/search_min.cpp
+++ challenges/challenge1/src/search_min.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
+#include <cmath>
 #include "search_min.hpp"
+
+double learningRate(double& alpha0, double& mu, int& methodLearningRate, int& k) {
+    double rate;
+
+    switch(methodLearningRate) {
+        case 0:
+            // Exponential decay
+            rate = alpha0 * std::exp(- mu * k);
+            break;
+        case 1:
+            // Inverse decay
+            rate = alpha0 / (1 + mu * k);
+            break;
+        case 2:
+            // Approximate line search with Armijo rule
+            rate = 2;
+            break;
+        default:
+            // Wrong value
+            std::cerr << "Wrong definition of the Learning rate in JSON file. Use of method 2." << std::endl;
+            rate = learningRate(alpha0, mu, 2, k);
+    }
+    return rate;
+}
 
 std::vector<double> searchMinimum(FunctionWrapper functionToMinimize, FunctionWrapperGradient functionGradient, Parameters& readParams) {
     std::cout << "Searching for the minimum..." << std::endl;
@@ -9,5 +34,7 @@ std::vector<double> searchMinimum(FunctionWrapper functionToMinimize, FunctionWr
         minPoint[i] = i;
     }
 
+    int k = 1;
+    double rate = learningRate(readParams.alpha0, readParams.mu, readParams.methodLearningRate, k);
     return minPoint;
 }
