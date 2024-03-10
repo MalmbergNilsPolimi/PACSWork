efbd7a7b723c3992501d869394c7840c2704d631
Nils Malmberg <nilsaido.malmberg@mail.polimi.it>
2024-03-10 21:40:45 +0000

    ADD: operations on vectors and learning rate

--- challenges/challenge1/include/search_min.hpp
+++ challenges/challenge1/include/search_min.hpp
@@ -7,7 +7,10 @@
 using FunctionWrapper = const std::function<double(const std::vector<double>&)>&;
 using FunctionWrapperGradient = const std::function<std::vector<double>(const std::vector<double>&)>&;
 
-double learningRate(double& alpha0, double& mu, int& methodLearningRate, int& k);
+double vectorNorm(const std::vector<double>& vect);
+std::vector<double> vectorDiff(const std::vector<double>& vect1, const std::vector<double>& vect2);
+std::vector<double> prodVectWithCst(const std::vector<double>& vect, const double& constant);
+double learningRate(double& alpha0, double& mu, int methodLearningRate, int& k, FunctionWrapper functionToMinimize, FunctionWrapperGradient functionGradient);
 std::vector<double> searchMinimum(FunctionWrapper functionToMinimize, FunctionWrapperGradient functionGradient, Parameters& readParams);
 
 #endif // SEARCH_MIN_HPP
