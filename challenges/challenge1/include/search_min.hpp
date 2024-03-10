c8d6f1755a193f675140256e40cc350afdd79363
Nils Malmberg <nilsaido.malmberg@mail.polimi.it>
2024-03-10 19:28:34 +0000

    ADD: function learning rate

--- challenges/challenge1/include/search_min.hpp
+++ challenges/challenge1/include/search_min.hpp
@@ -3,9 +3,11 @@
 
 #include <functional> // for std::function
 #include "main.hpp"
+
 using FunctionWrapper = const std::function<double(const std::vector<double>&)>&;
 using FunctionWrapperGradient = const std::function<std::vector<double>(const std::vector<double>&)>&;
 
+double learningRate(double& alpha0, double& mu, int& methodLearningRate, int& k);
 std::vector<double> searchMinimum(FunctionWrapper functionToMinimize, FunctionWrapperGradient functionGradient, Parameters& readParams);
 
 #endif // SEARCH_MIN_HPP
