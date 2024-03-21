13f05c1127246451f37a7db2462e98f4574026b7
Nils Malmberg <nilsaido.malmberg@mail.polimi.it>
2024-03-21 00:07:59 +0000

    ADD: cases for methodMinimization (case 0 and 2 work - not case 1)

--- challenges/challenge1/src/search_min.cpp
+++ challenges/challenge1/src/search_min.cpp
@@ -30,6 +30,12 @@ double learningRate(FunctionWrapper functionToMinimize, FunctionWrapperGradient 
             break;
         case 2:
             // Approximate line search with Armijo rule
+            if (readParams.methodMinimization==1) {
+                std::cerr << "You can't use the Armijo rule with the heavy-ball method. Use of exponential decay." << std::endl;
+                readParams.methodMinimization=0;
+                learningRate(functionToMinimize, functionGradient, readParams, vectXk, k);
+                break;
+            }
             while (functionToMinimize(vectXk) - functionToMinimize(vectorDiff(vectXk, prodVectWithCst(functionGradient(functionToMinimize ,vectXk, methodGradient), alpha0))) < sigma*alpha0*vectorNorm(functionGradient(functionToMinimize ,vectXk, methodGradient))*vectorNorm(functionGradient(functionToMinimize ,vectXk, methodGradient))) {
                 alpha0 /= 2;             
             }
@@ -50,6 +56,7 @@ double learningRate(FunctionWrapper functionToMinimize, FunctionWrapperGradient 
 
 
 std::vector<double> searchMinimum(FunctionWrapper functionToMinimize, FunctionWrapperGradient functionGradient, Parameters& readParams) {
+    static bool errorDisplayed = false;
     std::cout << "Searching for the minimum..." << std::endl;
 
     std::vector<double> vectX1((readParams.initialConditions).size());
@@ -58,15 +65,89 @@ std::vector<double> searchMinimum(FunctionWrapper functionToMinimize, FunctionWr
 
     int iter=0;
     double alphak = learningRate(functionToMinimize, functionGradient, readParams, vectX1, iter);
+    
+    // Initialization for case 0
     std::vector<double> vectX2 = vectorDiff(vectX1, prodVectWithCst(functionGradient(functionToMinimize ,vectX1, readParams.methodGradient), alphak));
-    ++iter;
+    
+    // Initialization for case 1
+    std::vector<double> vectd2((readParams.initialConditions).size());
 
-    while (iter <= readParams.maxIter && vectorNorm(vectorDiff(vectX2, vectX1)) >= readParams.lTol && vectorNorm(functionGradient(functionToMinimize ,vectX1, readParams.methodGradient)) >= readParams.rTol) {
-        std::copy(std::begin(vectX2), std::end(vectX2), std::begin(vectX1));
-        alphak = learningRate(functionToMinimize, functionGradient, readParams, vectX1, iter);
-        std::vector<double> tmpVect = vectorDiff(vectX1, prodVectWithCst(functionGradient(functionToMinimize ,vectX1, readParams.methodGradient), alphak));
-        std::copy(std::begin(tmpVect), std::end(tmpVect), std::begin(vectX2));
+    double eta;
+    if (alphak < 1) {
+            eta = 1 - alphak;
+    } else {
+            eta = 0.9;
+    }
+
+    std::vector<double> vectd1 = prodVectWithCst(functionGradient(functionToMinimize ,vectX1, readParams.methodGradient), -alphak);
+    
+    // Initialization for case 2
+    std::vector<double> vectY = vectorSum(vectX1, prodVectWithCst(vectorSum(vectX1, prodVectWithCst(vectX1, -1.)) , eta));
+
+    switch (readParams.methodMinimization)
+    {
+    case 0:
+        //Gradient method
         ++iter;
+        while (iter <= readParams.maxIter && vectorNorm(vectorDiff(vectX2, vectX1)) >= readParams.lTol && vectorNorm(functionGradient(functionToMinimize ,vectX1, readParams.methodGradient)) >= readParams.rTol) {
+            std::copy(std::begin(vectX2), std::end(vectX2), std::begin(vectX1));
+            alphak = learningRate(functionToMinimize, functionGradient, readParams, vectX1, iter);
+            std::vector<double> tmpVect = vectorDiff(vectX1, prodVectWithCst(functionGradient(functionToMinimize ,vectX1, readParams.methodGradient), alphak));
+            std::copy(std::begin(tmpVect), std::end(tmpVect), std::begin(vectX2));
+            ++iter;
+        }
+        break;
+    
+    case 1:
+        //Heavy-ball/momentum method
+        vectX2 = vectorSum(vectX1, vectd1);
+
+        while (iter <= readParams.maxIter && vectorNorm(vectorDiff(vectX2, vectX1)) >= readParams.lTol && vectorNorm(functionGradient(functionToMinimize ,vectX1, readParams.methodGradient)) >= readParams.rTol)
+        {
+            vectX2 = vectorSum(vectX1, vectd1);
+
+            ++iter;
+            alphak = learningRate(functionToMinimize, functionGradient, readParams, vectX1, iter);
+            
+            if (alphak < 1) {
+                eta = 1 - alphak;
+            } else {
+                eta = 0.9;
+            }
+            
+            vectd2 = vectorSum(prodVectWithCst(vectd1, eta), prodVectWithCst(functionGradient(functionToMinimize ,vectX2, readParams.methodGradient) , -alphak));
+            
+            std::copy(std::begin(vectX2), std::end(vectX2), std::begin(vectX1));
+            std::copy(std::begin(vectd2), std::end(vectd2), std::begin(vectd1));
+        }
+        
+        break;
+
+    case 2:
+        //Nesterov method
+        vectX2 = vectorSum(vectY, prodVectWithCst(functionGradient(functionToMinimize ,vectY, readParams.methodGradient), -alphak));        
+        
+        while (iter <= readParams.maxIter && vectorNorm(vectorDiff(vectX2, vectX1)) >= readParams.lTol && vectorNorm(functionGradient(functionToMinimize ,vectX1, readParams.methodGradient)) >= readParams.rTol)
+        {
+            ++iter;
+            alphak = learningRate(functionToMinimize, functionGradient, readParams, vectX2, iter);
+            if (alphak < 1) {
+                eta = 1 - alphak;
+            } else {
+                eta = 0.9;
+            }
+            std::copy(std::begin(vectX2), std::end(vectX2), std::begin(vectX1));
+            vectX2 = vectorSum(vectX1, prodVectWithCst(functionGradient(functionToMinimize ,vectX1, readParams.methodGradient), -alphak));
+            vectY = vectorSum(vectX2, prodVectWithCst(vectorSum(vectX2, prodVectWithCst(vectX1, -1.)) , eta));
+        }
+
+        break;
+
+    default:
+        std::cerr << "Wrong definition of the minimization method. Use of method 0." << std::endl;
+        readParams.methodMinimization = 0;
+        vectX2 = searchMinimum(functionToMinimize, functionGradient, readParams);
+        break;
     }
 
     return vectX2;
