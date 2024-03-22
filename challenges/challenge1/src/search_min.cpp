90584ccafaf117eb92d08b778df38f60958fd3e2
Nils Malmberg <nilsaido.malmberg@mail.polimi.it>
2024-03-22 18:13:33 +0000

    MODIF: optimize alpha case2 and rm case 1 2 search

--- challenges/challenge1/src/search_min.cpp
+++ challenges/challenge1/src/search_min.cpp
@@ -18,6 +18,7 @@ double learningRate(FunctionWrapper functionToMinimize, FunctionWrapperGradient 
     int methodGradient{readParams.methodGradient};
     double sigma{0.5};
     static bool errorDisplayed = false;
+    std::vector<double> gradient((readParams.initialConditions).size());
 
     switch(methodLearningRate) {
         case 0:
@@ -36,7 +37,8 @@ double learningRate(FunctionWrapper functionToMinimize, FunctionWrapperGradient 
                 learningRate(functionToMinimize, functionGradient, readParams, vectXk, k);
                 break;
             }
-            while (functionToMinimize(vectXk) - functionToMinimize(vectorDiff(vectXk, prodVectWithCst(functionGradient(functionToMinimize ,vectXk, methodGradient), alpha0))) < sigma*alpha0*vectorNorm(functionGradient(functionToMinimize ,vectXk, methodGradient))*vectorNorm(functionGradient(functionToMinimize ,vectXk, methodGradient))) {
+            gradient = functionGradient(functionToMinimize ,vectXk, methodGradient);
+            while (functionToMinimize(vectXk) - functionToMinimize(vectorDiff(vectXk, prodVectWithCst(gradient, alpha0))) < sigma*alpha0*vectorNorm(gradient)*vectorNorm(gradient)) {
                 alpha0 /= 2;             
             }
             rate = alpha0;
@@ -66,24 +68,8 @@ std::vector<double> searchMinimum(FunctionWrapper functionToMinimize, FunctionWr
     int iter=0;
     double alphak = learningRate(functionToMinimize, functionGradient, readParams, vectX1, iter);
     
-    // Initialization for case 0
     std::vector<double> vectX2 = vectorDiff(vectX1, prodVectWithCst(functionGradient(functionToMinimize ,vectX1, readParams.methodGradient), alphak));
     
-    // Initialization for case 1
-    std::vector<double> vectd2((readParams.initialConditions).size());
-
-    double eta;
-    if (alphak < 1) {
-            eta = 1 - alphak;
-    } else {
-            eta = 0.9;
-    }
-
-    std::vector<double> vectd1 = prodVectWithCst(functionGradient(functionToMinimize ,vectX1, readParams.methodGradient), -alphak);
-    
-    // Initialization for case 2
-    std::vector<double> vectY = vectorSum(vectX1, prodVectWithCst(vectorSum(vectX1, prodVectWithCst(vectX1, -1.)) , eta));
-
     switch (readParams.methodMinimization)
     {
     case 0:
@@ -100,46 +86,16 @@ std::vector<double> searchMinimum(FunctionWrapper functionToMinimize, FunctionWr
     
     case 1:
         //Heavy-ball/momentum method
-        vectX2 = vectorSum(vectX1, vectd1);
-
-        while (iter <= readParams.maxIter && vectorNorm(vectorDiff(vectX2, vectX1)) >= readParams.lTol && vectorNorm(functionGradient(functionToMinimize ,vectX1, readParams.methodGradient)) >= readParams.rTol)
-        {
-            vectX2 = vectorSum(vectX1, vectd1);
-
-            ++iter;
-            alphak = learningRate(functionToMinimize, functionGradient, readParams, vectX1, iter);
-            
-            if (alphak < 1) {
-                eta = 1 - alphak;
-            } else {
-                eta = 0.9;
-            }
-            
-            vectd2 = vectorSum(prodVectWithCst(vectd1, eta), prodVectWithCst(functionGradient(functionToMinimize ,vectX2, readParams.methodGradient) , -alphak));
-            
-            std::copy(std::begin(vectX2), std::end(vectX2), std::begin(vectX1));
-            std::copy(std::begin(vectd2), std::end(vectd2), std::begin(vectd1));
-        }
-        
+        std::cerr << "method 1 not defined. Use of method 0." << std::endl;
+        readParams.methodMinimization = 0;
+        vectX2 = searchMinimum(functionToMinimize, functionGradient, readParams);
         break;
 
     case 2:
         //Nesterov method
-        vectX2 = vectorSum(vectY, prodVectWithCst(functionGradient(functionToMinimize ,vectY, readParams.methodGradient), -alphak));        
-        
-        while (iter <= readParams.maxIter && vectorNorm(vectorDiff(vectX2, vectX1)) >= readParams.lTol && vectorNorm(functionGradient(functionToMinimize ,vectX1, readParams.methodGradient)) >= readParams.rTol)
-        {
-            ++iter;
-            alphak = learningRate(functionToMinimize, functionGradient, readParams, vectX2, iter);
-            if (alphak < 1) {
-                eta = 1 - alphak;
-            } else {
-                eta = 0.9;
-            }
-            std::copy(std::begin(vectX2), std::end(vectX2), std::begin(vectX1));
-            vectX2 = vectorSum(vectX1, prodVectWithCst(functionGradient(functionToMinimize ,vectX1, readParams.methodGradient), -alphak));
-            vectY = vectorSum(vectX2, prodVectWithCst(vectorSum(vectX2, prodVectWithCst(vectX1, -1.)) , eta));
-        }
+        std::cerr << "method 2 not defined. Use of method 0." << std::endl;
+        readParams.methodMinimization = 0;
+        vectX2 = searchMinimum(functionToMinimize, functionGradient, readParams);
         break;
 
     default:
